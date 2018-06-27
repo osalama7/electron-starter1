9d317ee3f4ccee38f7912e06ba59899fcc43cb0f
Osama Salama <osama.m7@gmail.com>
2018-06-27 15:21:17 +0200

    Camera app playground
    - adding gulp file watcher
    - adding electron connect
    - adding jquery
    - adding view
    - adding file structure

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+
+// Create an electron-connect server to enable reloading
+const electron = require('electron-connect').server.create();
+
+gulp.task('start', ()=>{
+	electron.start();
+	//Watch js files and restart Electron if they change
+	gulp.watch(['./app/js/*.js'], electron.restart);
+	//watch css files, but only reload (no restart necessary)
+	gulp.watch(['./app/css/*.css'], electron.reload);
+	//watch html
+	gulp.watch(['./index.html'], electron.reload);
+	//watch app
+	gulp.watch(['./app.js'], electron.reload);
+	//watch main
+	gulp.watch(['./main'], electron.reload);
+});
